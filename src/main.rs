0cc3a1d02362084fd9f244ba533d7282a13269e9
petru <smart__petea@mail.ru>
2022-01-01 20:37:54 +0200

    1/XII.2022

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use modular_bitfield::*;
+use std::ops::Index;
 
 type Byte = u8;
 type Word = u16;
@@ -6,64 +7,83 @@ type Word = u16;
 const MAX_MEM: usize = 1024 * 64;
 struct Mem
 {
-    Data: [Byte; MAX_MEM],
+    data: [Byte; MAX_MEM],
 }
 
 impl Mem {
     fn new() -> Self {
         Self {
-            Data: [Byte::default(); MAX_MEM],
+            data: [Byte::default(); MAX_MEM],
         }
     }
 
     fn initialize(&mut self) {
-        for data in self.Data.iter_mut() {
+        for data in self.data.iter_mut() {
             *data = Byte::default();
         }
     }
+}
 
+impl Index<u16> for Mem {
+    type Output=Byte;
+
+    fn index(&self, index: u16) -> &Byte {
+        &self.data[index as usize]
+    }
 }
 
 #[bitfield]
 struct CPU {
-    PC: Word, //program counter
-    SP: Word, //stack pointer
+    pc: Word, //program counter
+    sp: Word, //stack pointer
 
-    A: Byte, //registers
-    X: Byte, //registers
-    Y: Byte, //registers
+    a: Byte, //registers
+    x: Byte, //registers
+    y: Byte, //registers
 
-    C: specifiers::B1, //status flag
-    Z: specifiers::B1, //status flag
-    I: specifiers::B1, //status flag
-    D: specifiers::B1, //status flag
-    B: specifiers::B1, //status flag
-    V: specifiers::B1, //status flag
-    N: specifiers::B1, //status flag
-    N2Dummy: specifiers::B1, //status flag
+    c: specifiers::B1, //status flag
+    z: specifiers::B1, //status flag
+    i: specifiers::B1, //status flag
+    d: specifiers::B1, //status flag
+    b: specifiers::B1, //status flag
+    v: specifiers::B1, //status flag
+    n: specifiers::B1, //status flag
+    n2_dummy: specifiers::B1, //status flag
 }
 
 impl CPU {
     fn reset(&mut self, memory: &mut Mem)
     {
-        self.set_PC(0xFFFC);
-        self.set_SP(0x0100);
-        self.set_D(0);
-        self.set_A(0);
-        self.set_X(0);
-        self.set_Y(0);
+        self.set_pc(0xFFFC);
+        self.set_sp(0x0100);
+        self.set_d(0);
+        self.set_a(0);
+        self.set_x(0);
+        self.set_y(0);
 
-        self.set_C(0);
-        self.set_Z(0);
-        self.set_I(0);
-        self.set_B(0);
-        self.set_V(0);
-        self.set_N(0);
+        self.set_c(0);
+        self.set_z(0);
+        self.set_i(0);
+        self.set_b(0);
+        self.set_v(0);
+        self.set_n(0);
 
         memory.initialize();
     }
 
-    fn execute(&self, Ticks: u32, mem: &Mem) {
+    fn execute(&mut self, cycles: u32, memory: &Mem) {
+        let mut cycles = cycles;
+        while cycles > 0u32 {
+            let ins: Byte = self.fetch_byte(&mut cycles, memory);
+        }
+    }
+
+    fn fetch_byte(&mut self, cycles: &mut u32, memory: &Mem) -> Byte {
+        let data: Byte = memory[self.pc()];
+        self.set_pc(self.pc() + 1);
+        *cycles = *cycles - 1;
+
+        data
     }
 }
 
