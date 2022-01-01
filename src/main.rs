a44b486ac974cb4b2fc6764f1ee3296ee3293294
petru <smart__petea@mail.ru>
2022-01-01 18:48:18 +0200

    1/XII.2022

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,27 @@ use modular_bitfield::*;
 type Byte = u8;
 type Word = u16;
 
+const MAX_MEM: usize = 1024 * 64;
+struct Mem
+{
+    Data: [Byte; MAX_MEM],
+}
+
+impl Mem {
+    fn new() -> Self {
+        Self {
+            Data: [Byte::default(); MAX_MEM],
+        }
+    }
+
+    fn initialize(&mut self) {
+        for data in self.Data.iter_mut() {
+            *data = Byte::default();
+        }
+    }
+
+}
+
 #[bitfield]
 struct CPU {
     PC: Word, //program counter
@@ -23,7 +44,7 @@ struct CPU {
 }
 
 impl CPU {
-    fn reset(&mut self)
+    fn reset(&mut self, memory: &mut Mem)
     {
         self.set_PC(0xFFFC);
         self.set_SP(0x0100);
@@ -38,10 +59,17 @@ impl CPU {
         self.set_B(0);
         self.set_V(0);
         self.set_N(0);
+
+        memory.initialize();
+    }
+
+    fn execute(&self, Ticks: u32, mem: &Mem) {
     }
 }
 
 fn main() {
+    let mut mem: Mem = Mem::new();
     let mut cpu = CPU::new();
-    cpu.reset();
+    cpu.reset(&mut mem);
+    cpu.execute(2, &mut mem);
 }
